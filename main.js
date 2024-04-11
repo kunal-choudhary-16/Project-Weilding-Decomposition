a5c61ad29ce3be386b7bece17ae0097c83934beb
Kunal <135196470+kunal-choudhary-16@users.noreply.github.com>
2024-04-11 13:49:56 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Take input for z1
+var z1 = parseFloat(prompt("Enter the value for z1:"));
+
+// Take input for z2
+var z2 = parseFloat(prompt("Enter the value for z2:"));
+
+// Multiply z1 and z2
+var length = z1 * z2/2;
+
+// Output the length of the resulting value
+console.log("Length of the multiplication result:", length);
